9675fbc0ade13646b1e252388a4df8fae2a3c865
ncc02 <cuongtk2002@gmail.com>
2024-08-15 15:56:50 +0700

    tailwin css #3

---
+++ main.js
@@ -0,0 +1,16 @@
+const topMenu = document.getElementById('custom-top-menu');
+const toggleTopMenuIcon = document.getElementById('custom-toggle-top-menu-icon');
+
+document.addEventListener('click', (e) => {
+  if (toggleTopMenuIcon.contains(e.target)) {
+    //Click to Toggle top menu icon
+    topMenu.classList.toggle('hidden');
+  }else{
+    //Click outside from 
+    if (topMenu.classList.contains('custom-toggle-top-menu-icon')) {
+      topMenu.classList.remove('custom-top-menu');
+    }
+  }
+    
+}
+);
